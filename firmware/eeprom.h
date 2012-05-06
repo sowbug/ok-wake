206713018b14e15d40a467951f42f48f650b38c6
Mike Tsao <mike@sowbug.com>
2012-05-06 16:44:06 -0700

    Got rid of TODOs.

--- firmware/eeprom.h
+++ firmware/eeprom.h
@@ -8,16 +8,17 @@
 
 // Various sources say that a voltage problem can corrupt EEPROM address 0.
 // So we'll block it off and ignore it.
-uint32_t EEMEM kNeverUse = 0x55;
+uint8_t EEMEM kNeverUse = 0x55;
 
 // These values will determine the contents of the .eep file, which can be
 // used to program the EEPROM.
 uint8_t EEMEM kShouldSet = 0x01;
-uint8_t EEMEM kYear = 12;
-uint8_t EEMEM kMonth = 5;
-uint8_t EEMEM kDay = 15;
-uint8_t EEMEM kHour = 12;
-uint8_t EEMEM kMinute = 13;
-uint8_t EEMEM kWakeHour = 14;
-uint8_t EEMEM kWakeMinute = 15;
+uint8_t EEMEM kYear = M_YEAR;
+uint8_t EEMEM kMonth = M_MONTH;
+uint8_t EEMEM kDay = M_DAY;
+uint8_t EEMEM kHour = M_HOUR;
+uint8_t EEMEM kMinute = M_MINUTE;
+uint8_t EEMEM kSecond = M_SECOND;
+uint8_t EEMEM kWakeHour = 0x06;
+uint8_t EEMEM kWakeMinute = 0x00;
 
--- firmware/main.c
+++ firmware/main.c
@@ -106,7 +106,7 @@ static uint8_t maybe_set_rtc_time() {
                eeprom_read_byte(&kDay),
                eeprom_read_byte(&kHour),
                eeprom_read_byte(&kMinute),
-               0);
+               eeprom_read_byte(&kSecond) + 15);
 
   // Mark the time set.
   eeprom_update_byte(&kShouldSet, 0);
@@ -114,17 +114,49 @@ static uint8_t maybe_set_rtc_time() {
   return 1;
 }
 
+static uint8_t bcd_to_decimal(uint8_t bcd) {
+  uint8_t result = bcd & 0x0f;
+  return result + ((bcd >> 4) & 0x0f) * 10;
+}
+
+static uint8_t decimal_to_bcd(uint8_t decimal) {
+  uint8_t result = (decimal / 10) * 16;
+  return result + (decimal % 10);
+}
+
+static void refresh_time() {
+  hour = bcd_to_decimal(read_i2c_byte(RTC_ADDR, RTC_HOURS));
+  minute = bcd_to_decimal(read_i2c_byte(RTC_ADDR, RTC_MINUTES));
+  second = bcd_to_decimal(read_i2c_byte(RTC_ADDR, RTC_SECONDS));
+}
+
 static void read_wake_time() {
-  wake_hour = eeprom_read_byte(&kWakeHour);
-  wake_minute = eeprom_read_byte(&kWakeMinute);
+  wake_hour = bcd_to_decimal(eeprom_read_byte(&kWakeHour));
+  wake_minute = bcd_to_decimal(eeprom_read_byte(&kWakeMinute));
+}
+
+// The idea is you push the button at 6pm, and it'll set the clock for a
+// wakeup time of 6am.
+static void set_wake_time() {
+#define SET_WAKE_OFFSET_MINUTES (60 * 12)
+  int16_t now_plus = hour * 60 + minute + SET_WAKE_OFFSET_MINUTES;
+  while (now_plus >= (60 * 24)) {
+    now_plus -= 60 * 24;
+  }
+  wake_hour = now_plus / 60;
+  wake_minute = now_plus % 60;
+  eeprom_update_byte(&kWakeHour, decimal_to_bcd(wake_hour));
+  eeprom_update_byte(&kWakeMinute, decimal_to_bcd(wake_minute));
+
+  flicker_leds(1);
 }
 
 static void set_rtc_alarm() {
-  // TODO: derive time from wake_hour and wake_minute
+  // The (1 << 7) enables the alarm component.
   write_i2c_byte(RTC_ADDR, RTC_MINUTE_ALARM,
-                 (1 << 7) + (3 << 4) + (0));  // enabled, :30
+                 (1 << 7) + decimal_to_bcd(wake_minute));
   write_i2c_byte(RTC_ADDR, RTC_HOUR_ALARM,
-                 (1 << 7) + (0 << 4) + (5));  // enabled, 05:
+                 (1 << 7) + decimal_to_bcd(wake_hour));
   uint8_t rc1 = read_i2c_byte(RTC_ADDR, RTC_CONTROL_1);
   write_i2c_byte(RTC_ADDR, RTC_CONTROL_1, rc1 | (1 << 1));  // AIE=1
 }
@@ -138,11 +170,6 @@ static int16_t calculate_minutes_until_wake() {
 // Stop the default 32.768KHz CLKOUT signal on INT1.
 static void stop_32768_clkout() {
   write_i2c_byte(RTC_ADDR, RTC_TMR_CLKOUT_CTRL, RTC_CLKOUT_DISABLED);
-}
-
-static void set_wake_time() {
-  // TODO: set wake time to 12 hours from now.
-  flicker_leds(1);
 }
 
 static void set_second_interrupt(uint8_t enable) {
@@ -170,12 +197,6 @@ static uint8_t start_DAWN() {
   return STATE_DAWN;
 }
 
-static void refresh_time() {
-  hour = read_i2c_byte(RTC_ADDR, RTC_HOURS);
-  minute = read_i2c_byte(RTC_ADDR, RTC_MINUTES);
-  second = read_i2c_byte(RTC_ADDR, RTC_SECONDS);
-}
-
 static void power_down() {
   leds_off();
   init_power_reduction_register(1);
@@ -197,7 +218,7 @@ int main(void) {
   stop_32768_clkout();
   enable_pin_interrupts();
   if (maybe_set_rtc_time()) {
-    flicker_leds(4);
+    flicker_leds(16);
   }
   read_wake_time();
   set_rtc_alarm();
@@ -221,6 +242,11 @@ int main(void) {
                              minutes_until_wake < 0);
     uint8_t in_post_window = (minutes_until_wake >= 0 &&
                               minutes_until_wake < WAKE_WINDOW_POST_MINUTES);
+
+    //
+    in_pre_window = 0;
+    in_post_window = 1;
+    //
 
     if (in_pre_window && state != STATE_TWILIGHT) {
       state = start_TWILIGHT();
