2d82382293590f3106d71c6d0bbf3e1ed6f76769
Mike Tsao <mike@sowbug.com>
2012-05-08 10:17:56 -0700

    Got rid of '328 support. More optimizations.
    http://www.tty1.net/blog/2008-04-29-avr-gcc-optimisations_en.html

--- firmware/avr_device.h
+++ firmware/avr_device.h
@@ -8,14 +8,8 @@
 #if !defined(__AVR_DEVICE_H__)
 #define __AVR_DEVICE_H__
 
-#if defined(__AVR_ATtiny25__) || \
-  defined (__AVR_ATtiny45__) || \
-  defined (__AVR_ATtiny85__)
 #include "USI_TWI_Master.h"
 #include "attinyx5.h"
-#elif defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
-#include "atmega328.h"
-#endif
 
 static void init_ports() {
   // LEDs output
